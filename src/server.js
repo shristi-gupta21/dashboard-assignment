5c862762b77231dcc94395f02c5d9eede6ab2358
Shristi Gupta <gshristi40@gmail.com>
2024-02-25 14:56:31 +0530

    server.js file added

---
+++ src/server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+
+// Enable CORS for all requests
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
+const port = process.env.PORT || 4000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
